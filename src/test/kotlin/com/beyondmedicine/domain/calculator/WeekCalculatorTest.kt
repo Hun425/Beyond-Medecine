5db1425eee29a84f4d497cd909d9e92ed1c37f21
hun Mac <chae0738@naver.com>
2025-10-15 14:29:42 +0900

    test(calculator): 경계 조건 테스트 추가
    평가일이 활성화일 이전이거나 D+42 이후인 경우 null 반환 테스트

--- src/test/kotlin/com/beyondmedicine/domain/calculator/WeekCalculatorTest.kt
+++ src/test/kotlin/com/beyondmedicine/domain/calculator/WeekCalculatorTest.kt
@@ -77,4 +77,32 @@ class WeekCalculatorTest {
         // then
         assertEquals(6, weekNumber)
     }
+
+    @Test
+    @DisplayName("평가일이 활성화일보다 이전이면 null을 반환한다")
+    fun calculateWeekNumber_AssessmentBeforeActivation_ReturnsNull() {
+        // given
+        val activationDate = LocalDate.of(2025, 10, 1)
+        val assessmentDate = LocalDate.of(2025, 9, 30)  // 활성화 1일 전
+
+        // when
+        val weekNumber = WeekCalculator.calculateWeekNumber(activationDate, assessmentDate)
+
+        // then
+        assertEquals(null, weekNumber)
+    }
+
+    @Test
+    @DisplayName("활성화 후 42일(D+42)은 유효 기간 종료로 null을 반환한다")
+    fun calculateWeekNumber_AfterActiveWeeks_ReturnsNull() {
+        // given
+        val activationDate = LocalDate.of(2025, 10, 1)
+        val assessmentDate = LocalDate.of(2025, 11, 12)  // D+42
+
+        // when
+        val weekNumber = WeekCalculator.calculateWeekNumber(activationDate, assessmentDate)
+
+        // then
+        assertEquals(null, weekNumber)
+    }
 }
